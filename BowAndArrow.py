58966a686e6f5c29a8f9b0e707d2767d465c0d04
Sriharivignesh <srihari8@uw.edu>
2016-09-21 15:38:12 -0700

    Initialised necessary variables

---
+++ BowAndArrow.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+import os
+import random
+import numpy
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,200,0)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+GAME_FPS = 30
+
