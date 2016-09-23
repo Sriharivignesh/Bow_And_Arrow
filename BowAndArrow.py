c21e05bf766bc0599b1f61bd98d56687d8e8ebae
Sriharivignesh <srihari8@uw.edu>
2016-09-22 19:13:38 -0700

    Acquired mini sprite sheet

--- BowAndArrow.py
+++ BowAndArrow.py
@@ -3,6 +3,10 @@ import time
 import os
 import random
 import numpy
+import bisect
+import sys
+
+pygame.init()
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -17,16 +21,19 @@ BALLOON_GAME_COORDINATES_CONTAINER = []
 BALLOON_HEIGHT = 30
 BALLOON_WIDTH = 30
 BALLOON_GAME_HEIGHT = 600
-BALLOON_GAME_WIDTH = 300
+BALLOON_GAME_WIDTH = 280
+BALLOON_IMAGE = pygame.image.load("balloon.png")
+BALLOON_BURST_IMAGE = pygame.image.load("balloon_burst.png")
 
+GAME_BACKGROUND_IMAGE = pygame.image.load("background.jpg")
 
-ARROW_HEIGHT = 10
-ARROW_WIDTH = 10
+ARROW_HEIGHT = 2
+ARROW_WIDTH = 30
+ARROW_IMAGE = pygame.image.load("arrow.png")
 
 GAME_CONTINUE = True
 GAME_FPS = 30
 
-pygame.init()
 game_canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('BOW AND ARROW - RECREATION')
 
@@ -43,10 +50,14 @@ while GAME_CONTINUE:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GAME_CONTINUE = False
-    game_canvas.fill(GREEN)
+
+    #game_canvas.fill(GREEN)
+    game_canvas.blit(GAME_BACKGROUND_IMAGE, (0, 0))
     BALLOON_GAME_COORDINATES_CONTAINER = []
+    
     for y in range(17):
-        game_canvas.fill(RED,rect = [BALLOON_GAME_WIDTH,BALLOON_GAME_HEIGHT,10,10])
+        #game_canvas.fill(RED,rect = [BALLOON_GAME_WIDTH,BALLOON_GAME_HEIGHT,10,10])
+        game_canvas.blit(BALLOON_IMAGE,(BALLOON_GAME_WIDTH,BALLOON_GAME_HEIGHT))
         BALLOON_GAME_COORDINATES_CONTAINER.append(BALLOON_GAME_WIDTH)
         BALLOON_GAME_WIDTH += 30
     
@@ -57,7 +68,7 @@ while GAME_CONTINUE:
     BALLOON_GAME_HEIGHT -= 5
     if BALLOON_GAME_HEIGHT <= 0:
         BALLOON_GAME_HEIGHT = SCREEN_HEIGHT
-    BALLOON_GAME_WIDTH = 300
+    BALLOON_GAME_WIDTH = 280
     game_clock.tick(GAME_FPS)
 
 
